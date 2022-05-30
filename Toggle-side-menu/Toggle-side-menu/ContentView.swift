135b1990345a39002b74d3e87dde2a0b0c229d21
Ruslan <forgetruslan@gmail.com>
2022-05-30 13:38:36 +0300

    added custom side menu

--- Toggle-side-menu/Toggle-side-menu/ContentView.swift
+++ Toggle-side-menu/Toggle-side-menu/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State var isChoosing = false
+    @State var offset: CGFloat = 400.0
+    
+    var menuStack: some View {
+        ZStack(alignment: .topLeading) {
+            RoundedRectangle(cornerRadius: 16).fill(Color.yellow)
+            VStack(alignment: .leading, spacing: 20) {
+                makeButton(buttonText: "Profile")
+                makeButton(buttonText: "Map")
+                makeButton(buttonText: "Support")
+                makeButton(buttonText: "Exit")
+            }
+        }
+    }
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 16).fill(Color.mint).offset(x: isChoosing ? 100 : 0)
+        VStack {
+            ZStack {
+                menuStack
+                RoundedRectangle(cornerRadius: 16).fill( isChoosing ? Color.mint : Color.red).offset(x: isChoosing ? 200 : 0)
+            }
             Toggle(isOn: $isChoosing, label: {
-                Text("Вы действительно этого хотите?")
-            }).padding().offset(x: isChoosing ? 50 : -50)
-        }
+                Text("Do you want to change View's color")
+            }).padding()
+        }.animation(Animation.spring(response: 0.5,
+                                     dampingFraction: 0.5,
+                                     blendDuration: 0.5))
+    }
+    
+    func makeButton(buttonText: String) -> some View {
+        Button(action: { self.isChoosing = false },
+               label: { Text(buttonText).font(.system(size: 30)) }).padding()
     }
 }
 
