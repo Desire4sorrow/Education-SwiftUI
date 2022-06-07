cd57357df96a3e4e0e72a52b0c37aa222667bbfb
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-06-07 12:39:18 +0300

    added prototype

---
+++ TabViewWithBinding/TabViewWithBinding/TabViewWithBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewWithBindingApp.swift
+//  TabViewWithBinding
+//
+//  Created by Omega on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TabViewWithBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
