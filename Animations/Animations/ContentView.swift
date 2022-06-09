5014b720fddb79f764ad4a56a4c1d127772521ff
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-06-09 16:27:00 +0300

    worked with animation of button and image.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var animationAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("lancer-evo", bundle: nil)
+                .resizable()
+                .frame(width: 300, height: 250)
+                .padding()
+                .overlay() {
+                    RoundedRectangle(cornerRadius: 1)
+                        .stroke(.red)
+                        .scaleEffect(animationAmount)
+                        .opacity(2 - animationAmount)
+                }
+                .animation(.easeInOut, value: animationAmount)
+            Button("Tap here") {
+                
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 2)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount)
+            )
+            .onAppear {
+                animationAmount = 2
+            }
+        }
     }
 }
 
