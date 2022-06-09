4e78e558985f84b2cfaa9e8cbd4f0dba3c238a50
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-06-09 14:42:35 +0300

    added base struct.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Omega on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
